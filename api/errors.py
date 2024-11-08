2adcb9e29e621e0e73a354ae924988843368fafc
talal <talalbadreddine@hotmail.com>
2024-11-08 16:33:30 +0200

    fix: replacing error dict with string

--- api/errors.py
+++ api/errors.py
@@ -1,10 +1,10 @@
 class APIErrorMessages:
-    INVALID_CLIENT_ID = {'error': 'Invalid client_id'}
-    USER_NOT_FOUND = {'error': 'User does not exist'}
-    INVALID_CREDENTIALS = {'error': 'Invalid username or password.'}
-    INTERNAL_SERVER_ERROR = {'error': 'Internal Server Error'}
-    INVALID_INPUT = {'error': 'Invalid input'}
-    USERNAME_EXISTS = {'error': 'A user with this username already exists.'}
-    PASSWORD_TOO_SHORT = {'error': 'Password must be at least 8 characters long.'}
-    INVALID_DATE_FORMAT = {'error': 'Invalid date format. Please use YYYY-MM-DD.'}
-    START_DATE_BEFORE_END_DATE = {'error': 'start_date must be before end_date.'}
+    INVALID_CLIENT_ID = 'Invalid client_id'
+    USER_NOT_FOUND = 'User does not exist'
+    INVALID_CREDENTIALS = 'Invalid username or password.'
+    INTERNAL_SERVER_ERROR = 'Internal Server Error'
+    INVALID_INPUT = 'Invalid input'
+    USERNAME_EXISTS = 'A user with this username already exists.'
+    PASSWORD_TOO_SHORT = 'Password must be at least 8 characters long.'
+    INVALID_DATE_FORMAT = 'Invalid date format. Please use YYYY-MM-DD.'
+    START_DATE_BEFORE_END_DATE = 'start_date must be before end_date.'
