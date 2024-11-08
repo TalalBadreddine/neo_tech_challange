b8b570338dc074cb4887d1969e59a770514960f3
talal <talalbadreddine@hotmail.com>
2024-11-08 11:46:07 +0200

    feat: added signle row etl processing option

--- etl/management/commands/run_etl.py
+++ etl/management/commands/run_etl.py
@@ -7,13 +7,14 @@ class Command(BaseCommand):
     def add_arguments(self, parser):
         parser.add_argument('--clients-file', type=str, help='Path to clients CSV/Excel file')
         parser.add_argument('--transactions-file', type=str, help='Path to transactions CSV/Excel file')
+        parser.add_argument('--single-row-processing', action='store_true', help='Inserts row individually into db')
 
     def handle(self, *args, **options):
         if options['clients_file']:
-            task = process_clients_file.delay(options['clients_file'])
+            task = process_clients_file.delay(options['clients_file'], single_row_processing=options['single_row_processing'])
             self.stdout.write(self.style.SUCCESS(f'Processing clients file: {options["clients_file"]}. Task ID: {task.id}'))
 
 
         if options['transactions_file']:
-            task = process_transactions_file.delay(options['transactions_file'])
+            task = process_transactions_file.delay(options['transactions_file'], single_row_processing=options['single_row_processing'])
             self.stdout.write(self.style.SUCCESS(f'Processing transactions file: {options["transactions_file"]}. Task ID: {task.id}'))
--- etl/tasks.py
+++ etl/tasks.py
@@ -5,20 +5,39 @@ from core.models import Client, Transaction
 from core.models.transaction_statistics_view import TransactionStatistics
 from .processors import ClientProcessor, DataProcessor, TransactionProcessor
 
-# TODO: decide if we want to handle each row individually or in bulk, to not roll back all rows on failure
 @shared_task
-def process_file(file_path: str, model, processor: DataProcessor) -> dict:
+def process_file(file_path: str, model, processor: DataProcessor, single_row_processing=False, chunk_size=1000) -> dict:
     try:
         df = pd.read_csv(file_path) if file_path.endswith('.csv') else pd.read_excel(file_path)
         df = processor.clean_dataframe(df)
 
         processed_count = 0
-        with transaction.atomic():
-            for _, row in df.iterrows():
+        errors = []
+        valid_data = []
+
+        for _, row in df.iterrows():
+            try:
                 processed_data = processor.process_row(row)
                 if processed_data:
-                    model.objects.update_or_create(**processed_data)
+                    valid_data.append(processed_data)
+                else:
+                    errors.append({'row': row.to_dict(), 'error': 'Invalid data.'})
+            except Exception as e:
+                errors.append({'row': row.to_dict(), 'error': str(e)})
+
+        if single_row_processing:
+            for data in valid_data:
+                try:
+                    model.objects.update_or_create(**data)
                     processed_count += 1
+                except Exception as e:
+                    errors.append({'row': row.to_dict(), 'error': str(e)})
+        else:
+            with transaction.atomic():
+                for i in range(0, len(valid_data), chunk_size):
+                    chunk = valid_data[i:i + chunk_size]
+                    model.objects.bulk_create([model(**data) for data in chunk], ignore_conflicts=True)
+                    processed_count += len(chunk)
 
         TransactionStatistics.refresh()
         return {
@@ -34,11 +53,11 @@ def process_file(file_path: str, model, processor: DataProcessor) -> dict:
         }
 
 @shared_task
-def process_clients_file(file_path: str) -> dict:
+def process_clients_file(file_path: str, single_row_processing=False) -> dict:
     processor = ClientProcessor()
-    return process_file(file_path, Client, processor)
+    return process_file(file_path, Client, processor, single_row_processing)
 
 @shared_task
-def process_transactions_file(file_path: str) -> dict:
+def process_transactions_file(file_path: str, single_row_processing=False) -> dict:
     processor = TransactionProcessor()
-    return process_file(file_path, Transaction, processor)
+    return process_file(file_path, Transaction, processor, single_row_processing)
